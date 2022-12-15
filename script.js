4c3b54819944f5d58a5f643cad9a782c2ca5e48c
LidaChk <lvglazko@gmail.com>
2022-12-15 17:47:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.querySelector(".container").addEventListener("click", (e) => {
+  const t = e.target;
+  const from = t.closest(".container").querySelector(".from");
+  const to = t.closest(".banka").querySelector(".water");
+  console.log("-----------------------------");
+  console.log("-----------------------------");
+  console.log(!!from, !!to);
+  console.log("-----------------------------");
+  if (from) {
+    let toV = parseInt(to.dataset.value);
+    let freeV = parseInt(t.closest(".banka").dataset.size) - toV;
+    let fromV = parseInt(from.dataset.value);
+    console.log("toV, freeV, fromV", toV, freeV, fromV);
+    if (freeV >= fromV) {
+      from.dataset.value = 0;
+      to.dataset.value = fromV + toV;
+    } else {
+      from.dataset.value = fromV - freeV;
+      to.dataset.value = toV + freeV;
+    }
+    from.classList.toggle("from");
+  } else if (to) to.classList.toggle("from");
+
+  console.log(t.closest(".water"));
+  console.log(t.closest(".banka"));
+});
